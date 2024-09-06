f6c3eea4bda09ad322207dd68abb2ef39716769e
Facundo Colombo <facucolombo@gmail.com>
2024-09-06 14:44:46 -0300

    Initial Commit

---
+++ 1-Figuritas/Figus.py
@@ -0,0 +1,69 @@
+import random
+#Facundo Colombo
+def llenar_album(cantfigus):
+    album = [0] * cantfigus
+    contador_figus = 0
+
+    while (sum(album) < cantfigus):
+        album = pegar_figu(album,cantfigus)
+        contador_figus = contador_figus + 1
+
+    return album
+
+def pegar_figu(album,cantfigus):
+    figu = random.randint(0, cantfigus - 1)
+    album[figu] = 1
+    return album
+
+def cuantas_figus_para_llenar_album(figus_total):
+
+    album = [0] * figus_total
+    contador_albumes = 0
+
+    while (sum(album) < figus_total):
+        album = pegar_figu(album, figus_total)
+        contador_albumes += 1
+
+    return contador_albumes
+
+def cuantas_figus_para_llenar_albumes(figus_total, n_albumes):
+
+    albumesActuales = [0] * n_albumes
+    contador = 0
+    while(len(albumesActuales) > contador):
+        albumesActuales[contador] = cuantas_figus_para_llenar_album(figus_total)
+        contador += 1
+
+    return albumesActuales
+
+def promedio_figus_para_llenar_album(figus_total, n_albumes):
+
+    resultado = sum(cuantas_figus_para_llenar_albumes(figus_total,n_albumes))
+    return resultado / n_albumes
+# album lleno => lista de ints. cantidad maxima => int
+def chance_llenar_album(resultados_album_lleno, cantidad_maxima_figus):
+
+    index = 0
+    resultadosFavorables = 0
+    while(len(resultados_album_lleno)>index):
+
+         elem = resultados_album_lleno[index]
+         index += 1
+         if(elem <= cantidad_maxima_figus):
+             resultadosFavorables += 1
+
+    return (resultadosFavorables / cantidad_maxima_figus) * 100.0 #float
+#lo multiplico por 100 ya que el resultado sino estaria normalizado
+
+
+def llenar_y_devolver_chance(figus_total_album, cantidad_maxima_figus, n_albumes):
+
+
+    album = [0] * figus_total_album
+
+    repeticionActual =n_albumes
+    while(repeticionActual > 0):
+         
+
+    chance_llenar_album(album,cantidad_maxima_figus)
+    return 0
---
+++ 2- 10 mil/Diez Mil Facundo Colombo Avance Clase.py
@@ -0,0 +1,117 @@
+# suman puntos los unos y cinco
+# se empieza con 0 punto
+# en cada ronda cada jugador realiza una jugada
+# por cada 1 obtenido se suman 100 puntos
+# por cada 5 obtenido se suman 50 puntos
+# se podria usar una tupla o diccionario para redirigir los valores(?)
+# bucle: hasta que alguno llegue a 10 000
+# tirar dados
+# tiro dados
+# anotar resultados
+# sumo resultados
+# chequeo si alguien gano
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  6 07:27:33 2024
+
+@author: Facundo Colombo
+"""
+import random
+
+
+def tirar_cubilete():
+    # se puede ir al reves? de 5 a 0?
+    cubilete = [0] * 6
+    for i in range(0, len(cubilete) - 1):
+        cubilete[i] = random.randint(1, 6)
+
+    return cubilete
+
+
+def cuantos_hay(elemento, lista):
+    cantidad = 0
+
+    for num in lista:
+        if num == elemento:
+            cantidad += 1
+
+    return cantidad
+
+
+# por cada uno obtenido, se suman 100 puntos, mientras que 3 unos suman 1000 puntos, 4
+# unos suman 1100, y 5 unos suman 10000 puntos.
+
+# es una buena practica tener un solo return en las funciones en lo posible,
+# lo tengo en cuenta para las siguientes ejercitacions / casos donde me encuentre este problema
+def puntos_por_unos(lista_dados):
+    cantidad = cuantos_hay(1, lista_dados)
+
+    if cantidad <= 0: return 0
+
+    if cantidad < 3: return cantidad * 100
+
+    if cantidad < 5: return 1000 + (cantidad - 3) * 100
+
+    return 10000
+
+
+# por cada cinco obtenido se suman 50, mientras
+# que 3 cincos suman 500, 4 cincos suman 550, y 5 cincos suman 600 puntos
+# se podria ver alguna otra forma de agregar condiciones infinitas?
+def puntos_por_cincos(lista_dados):
+    cantidad = cuantos_hay(5, lista_dados)
+
+    if cantidad <= 0: return 0
+
+    if cantidad < 3: return cantidad * 50
+
+    if cantidad < 5: return 500 + (cantidad - 3) * 50
+
+    return 600
+
+
+def total_puntos(lista_dados):
+    return puntos_por_unos(lista_dados) + puntos_por_cincos(lista_dados)
+
+
+def hay_10mil(puntajes_jugadores):
+    for puntaje in puntajes_jugadores:
+        if puntaje >= 10000: return True
+
+    return False
+
+
+def acumular_puntos(puntajes_acumulados, puntajes_ronda_actual):
+    for i in range(len(puntajes_acumulados)):
+        puntajes_acumulados[i] += puntajes_ronda_actual[i]
+
+    return puntajes_acumulados
+
+
+def jugar_ronda(cant_jugadores):
+    puntajes_ronda = [0] * cant_jugadores
+    for i in range(cant_jugadores):
+        puntajes_ronda[i] = total_puntos(tirar_cubilete())
+
+    return puntajes_ronda
+
+
+def partida_completa(cant_jugadores):
+    puntajes_jugadores = [0] * cant_jugadores
+    rondas_jugadas = 0
+    while not hay_10mil(puntajes_jugadores):  # deberia poner un watchdog por las dudas
+        puntajes_ronda = jugar_ronda(cant_jugadores)
+        puntajes_jugadores = acumular_puntos(puntajes_jugadores, puntajes_ronda)
+        rondas_jugadas += 1
+
+    return rondas_jugadas
+
+
+def cant_rondas_promedio(cant_jugadores, cant_partidas):
+    cant_rondas = 0
+    for _ in range(0, cant_partidas):
+        cant_rondas += partida_completa(cant_jugadores)
+
+
+    return cant_rondas / cant_partidas
